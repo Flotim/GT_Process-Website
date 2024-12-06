354b9bd542f6b570a5ffea52f1771d89802a5447
Tim <bgltitmeo@gmail.com>
2024-12-06 20:29:13 +0100

    commit animation

--- script.js
+++ script.js
@@ -25,4 +25,30 @@ window.addEventListener('load', function() {
 });
 
 
+// Sélectionner toutes les sections
+const sections = document.querySelectorAll("section");
+
+// Créer un observer pour détecter quand une section entre dans le viewport
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Ajouter la classe "visible" pour activer l'animation
+        entry.target.classList.add("visible2");
+
+        // Si l'on souhaite observer une seule fois :
+        observer.unobserve(entry.target); // Stoppe l'observation
+      }
+    });
+  },
+  {
+    threshold: 0.3 // Déclenche quand 20% de la section est visible
+  }
+);
+
+// Appliquer l'observer à chaque section
+sections.forEach(section => {
+  observer.observe(section);
+});
+
 
